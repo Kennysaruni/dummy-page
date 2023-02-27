5071b368c4b3dde7cbb77db2e2fd76ee688925d8
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-02-28 00:19:20 +0300

    Dummy Page

--- src/components/EventsNavBar.js
+++ src/components/EventsNavBar.js
@@ -13,11 +13,11 @@ function EventsNavBar() {
 
         <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">EventsHub</Navbar.Brand>
+          <Navbar.Brand href="/">Donate It</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/eventsavailable">View Events</Nav.Link>
-            <Nav.Link href="/addevent">Add Event</Nav.Link>
+            <Nav.Link href="/eventsavailable">View  Donations</Nav.Link>
+            <Nav.Link href="/addevent">Add Donation</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
